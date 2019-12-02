caa52e3af4f5abc0b1e42e8ee6a4c1968b03e3c2
HAOYUatHZ <haoyu@protonmail.com>
2019-12-02 15:58:13 +0800

    pause

--- main.go
+++ main.go
@@ -17,6 +17,23 @@ func main() {
 
 	party1 := tss.NewPartyID("1", "moniker1", p2pPubkey1)
 	party2 := tss.NewPartyID("2", "moniker1", p2pPubkey2)
-	fmt.Println(party1)
-	fmt.Println(party2)
+	// fmt.Println(party1)
+	// fmt.Println(party2)
+
+	parties := make([]*tss.PartyID, 2)
+	parties[0] = party1
+	parties[1] = party2
+
+	partyIDMap := make(map[string]*tss.PartyID)
+	for _, id := range parties {
+		partyIDMap[id.Id] = id
+	}
+	fmt.Println(partyIDMap)
+
+	ctx := tss.NewPeerContext(tss.SortPartyIDs(parties))
+	params := tss.NewParameters(ctx, party1, len(parties), 1)
+	preParams, _ := keygen.GeneratePreParams(1 * time.Minute)
+
+	// TODO:
+	party := keygen.NewLocalParty(params, outCh, endCh, preParams)
 }
