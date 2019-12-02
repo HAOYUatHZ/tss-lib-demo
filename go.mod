a724e23751d7c29f91ad1e7ee508b4e61f8e3cb9
HAOYUatHZ <haoyu@protonmail.com>
2019-12-02 15:51:29 +0800

    gogogo

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/HAOYUatHZ/tss-lib-demo
+
+go 1.12
+
+require github.com/binance-chain/tss-lib v1.1.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
 import (
-	"github.com/binance-chain/tss-lib"
+	"fmt"
+	"math/big"
+
+	"github.com/binance-chain/tss-lib/tss"
 )
+
+func main() {
+
+	p2pPubkey1 := big.NewInt(1)
+	p2pPubkey2 := big.NewInt(2)
+
+	fmt.Println(p2pPubkey1)
+	fmt.Println(p2pPubkey2)
+
+	party1 := tss.NewPartyID("1", "moniker1", p2pPubkey1)
+	party2 := tss.NewPartyID("2", "moniker1", p2pPubkey2)
+	fmt.Println(party1)
+	fmt.Println(party2)
+}
